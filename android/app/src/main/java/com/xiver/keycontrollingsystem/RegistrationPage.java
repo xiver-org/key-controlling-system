fe0ed38a6300626f234fdf51395f4aec2a6776f7
GigantPro <pochtagigantpro@gmail.com>
2024-05-19 15:25:14 +0300

    Init backend

--- android/app/src/main/java/com/xiver/keycontrollingsystem/RegistrationPage.java
+++ android/app/src/main/java/com/xiver/keycontrollingsystem/RegistrationPage.java
@@ -10,13 +10,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class RegistrationPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_registration_page);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -24,13 +24,13 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void loginBtnTriggerr(View v) {
-        Intent intent = new Intent(this, MainPage.class);
+    public void loginTextTriggerr(View v) {
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 
-    public void registrationTextTriggerr(View v) {
-        Intent intent = new Intent(this, RegistrationPage.class);
+    public void registrationBtnTriggerr(View v) {
+        Intent intent = new Intent(this, MainPage.class);
         startActivity(intent);
     }
 }
